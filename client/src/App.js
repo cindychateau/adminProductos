b9f47885249db8c425ee2307ede434ddd0deb3bc
Cynthia Castillo <ca.castillo@gmail.com>
2022-05-09 12:54:19 -0500

    Administrador de Productos

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import TodosProductos from "./componentes/TodosProductos";
+import NuevoProducto from "./componentes/NuevoProducto";
+import Producto from "./componentes/Producto";
+
+const App = () => {
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact render={()=> <TodosProductos />} />
+          <Route path="/nuevo" render={() => <NuevoProducto />} />
+          <Route path="/producto/:id" render={()=> <Producto />} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
